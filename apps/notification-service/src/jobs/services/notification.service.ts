22f0103f0ce58e37494b37d9eb0e83887a4ec18d
Nouu188 <dg.pthinh@gmail.com>
2025-09-08 21:00:24 +0700

    Debug notification service due to incorrect import

--- apps/notification-service/src/jobs/services/notification.service.ts
+++ apps/notification-service/src/jobs/services/notification.service.ts
@@ -6,6 +6,7 @@ import { DoseStatus } from 'apps/scheduling-service/src/doses/entities/dose.enti
 import { DataSource } from 'typeorm';
 import { FirebaseService } from '../../firebase/firebase.service';
 import { NotificationStatus, NotificationType } from '../../notification-history/entities/notification-history.entity';
+import { NotificationHistoryService } from '../../notification-history/notification-history.service';
 
 interface GroupedNotificationPayload {
   userId: string;
@@ -20,7 +21,7 @@ export class NotificationService {
     private readonly doseApiClient: DoseApiClientService,
     private readonly accountApiClient: AccountApiClientService,
     private readonly firebaseService: FirebaseService,
-    private readonly historyService: Notification,
+    private readonly historyService: NotificationHistoryService,
     @InjectDataSource('notificationConnection') private readonly dataSource: DataSource, 
   ) {}
 
